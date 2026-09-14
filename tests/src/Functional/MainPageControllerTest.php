eb91a92ba180532c4713b5d6a480290dd75d77f0
Ambient.Impact <i@ambientimpact.com>
2026-09-14 19:41:40 -0400

    neurocracy/omnipedia/omnipedia#81: Use minimal profile for Drupal 11.4 tests.

--- tests/src/Functional/MainPageControllerTest.php
+++ tests/src/Functional/MainPageControllerTest.php
@@ -53,6 +53,15 @@ class MainPageControllerTest extends BrowserTestBase {
 
   /**
    * {@inheritdoc}
+   *
+   * @see https://gitlab.com/neurocracy/omnipedia/omnipedia/-/work_items/81
+   *   Don't use the core 'testing' profile since it provides an incompatible
+   *   node body storage config in drupal/core:^11.4.
+   */
+  protected $profile = 'minimal';
+
+  /**
+   * {@inheritdoc}
    */
   protected $defaultTheme = 'stark';
 
--- tests/src/Functional/WikiNodeEditLocalTaskTest.php
+++ tests/src/Functional/WikiNodeEditLocalTaskTest.php
@@ -55,6 +55,15 @@ class WikiNodeEditLocalTaskTest extends BrowserTestBase {
 
   /**
    * {@inheritdoc}
+   *
+   * @see https://gitlab.com/neurocracy/omnipedia/omnipedia/-/work_items/81
+   *   Don't use the core 'testing' profile since it provides an incompatible
+   *   node body storage config in drupal/core:^11.4.
+   */
+  protected $profile = 'minimal';
+
+  /**
+   * {@inheritdoc}
    */
   protected $defaultTheme = 'stark';
 
